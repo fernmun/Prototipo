9d1bcf78da71b9a450fccaf96d81672d0ec24454
David Nova <david4lim@gmail.com>
2012-04-29 16:39:53 -0500

    main

--- src/logica/Prototipo.java
+++ src/logica/Prototipo.java
@@ -4,6 +4,17 @@
  */
 package logica;
 
+import edu.logica.CommandExecutor;
+import edu.logica.KeyStoreTools;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+
 /**
  *
  * @author lmparra
@@ -13,7 +24,22 @@ public class Prototipo {
   /**
    * @param args the command line arguments
    */
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     // TODO code application logic here
+      CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+
+        FileInputStream fis = new FileInputStream("google.cer");
+
+        Certificate cert = certFactory.generateCertificate(fis);
+        fis.close();
+        
+//        System.out.println(cert);
+        
+        KeyStoreTools kstPrueba = new KeyStoreTools();
+        kstPrueba.createKeyStore("ksPrueba", "password");
+        kstPrueba.loadKeyStore("ksPrueba", "password");
+        kstPrueba.addCertificate("google", cert, "password");
+        Certificate cerGoogle = kstPrueba.getCertificate("google");
+        System.out.println(cerGoogle);
   }
 }
