81e90a81b3c2bc2e6c39aea3b0dd10b970c40150
David Nova <david4lim@gmail.com>
2012-04-29 16:29:38 -0500

    Commit

---
+++ src/logica/CommandExecutor.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.logica;
+
+import java.io.*;
+
+/**
+ *
+ * @author dnova
+ */
+public class CommandExecutor {
+    
+    private String SO;
+    
+    public CommandExecutor(String SO){
+        this.SO = SO;
+    }
+    
+    public void executeCommand(String command){
+        String s = "";
+
+        try {
+            
+	    // run the Unix "ps -ef" command
+            // using the Runtime exec method:
+            Process p = Runtime.getRuntime().exec(command);
+            
+            BufferedReader stdInput = new BufferedReader(new 
+                 InputStreamReader(p.getInputStream()));
+
+            BufferedReader stdError = new BufferedReader(new 
+                 InputStreamReader(p.getErrorStream()));
+
+            // read the output from the command
+            System.out.println("Here is the standard output of the command:\n");
+            String strTemp;
+            while ((strTemp = stdInput.readLine()) != null) {
+                s += strTemp;
+            }
+            System.out.println(s);
+            // read any errors from the attempted command
+            System.out.println("Here is the standard error of the command (if any):\n");
+            while ((s = stdError.readLine()) != null) {
+                System.out.println(s);
+            }
+            
+            System.exit(0);
+        }
+        catch (IOException e) {
+            System.out.println("exception happened - here's what I know: ");
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+}
