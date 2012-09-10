952b09535da67ecf3f79cad249a542e5234df2de
lfmparra <lfmparra@gmail.com>
2012-09-09 20:45:40 -0500

    Se crea la clase para crear las ventanas necesarias y además una clase para iniciar la lógica llamada Start

--- src/edu/view/FrameClient.java
+++ src/edu/view/FrameClient.java
@@ -12,6 +12,15 @@ import  javax.swing.JFrame;
  */
 public class FrameClient extends JFrame{
   
+  private int width, height, posX, posY;
+  private String title;
   
+  public FrameClient(int width, int height, String title, int posX, int posY) {
+    // Set all needed properties
+    this.setBounds(posX, posY, width, height);
+    this.setTitle(title);
+    this.setVisible(true);
+    
+  }
   
 }
---
+++ src/edu/view/Start.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.view;
+
+/**
+ *
+ * @author lmparra
+ */
+public class Start {
+  
+  public static void main(String args[]) {
+    FrameClient frame = new FrameClient(200, 300, "Ventana de prueba", 800, 200);
+  }
+  
+}
