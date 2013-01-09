7f4e3d1d0c8a4e3ee0b5cb54e59ccfe9db03a8ac
lmparra <lfmparra@gmail.com>
2013-01-08 21:04:28 -0500

    Se cambia el nombre de PropertieTool a PropertiesTool

---
+++ src/edu/logic/PropertiesTool.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.logic;
+
+import edu.view.SignSendButton;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author lmparra
+ * 
+ * The <code>PropertiesTool</code> Class extends <code>Properties</code> to 
+ * make easier instance way.
+ */
+public class PropertiesTool extends Properties{
+    
+    private InputStream inputStream;
+    
+    public PropertiesTool(String fileName) throws IOException {
+        try {
+            inputStream = new FileInputStream(fileName);
+            this.load(inputStream);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(PropertiesTool.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
