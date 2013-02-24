4b6fa77c98f1cb4ff5eedbd184766b9e5280d7b1
david <david@dnova-XPS-15Z>
2013-02-24 11:41:47 -0500

    modificación de firmado

---
+++ src/edu/api/SignerCreator.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.api;
+
+/**
+ *
+ * @author david
+ */
+public interface SignerCreator {
+    public Signer getSigner(String s);
+}
