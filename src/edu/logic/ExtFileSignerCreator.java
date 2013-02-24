909ea446e633063ac565ef194eeecd605319d32e
david <david@dnova-XPS-15Z>
2013-02-24 11:38:58 -0500

    modificación de firmado

---
+++ src/edu/logic/ExtFileSignerCreator.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.logic;
+
+import edu.api.Signer;
+import edu.api.SignerCreator;
+
+/**
+ *
+ * @author david
+ */
+public class ExtFileSignerCreator implements SignerCreator{
+
+    @Override
+    public Signer getSigner(String fileExt) {
+        
+        Signer signer;
+        
+        if(fileExt.equals("zip")){
+            signer = new FileSigner();
+        }
+        else if(fileExt.equals("pdf")){
+            signer = new PDFSigner();
+        }
+        else{
+            signer = new FileSigner();
+        }
+        
+        return signer;
+    }
+    
+}
