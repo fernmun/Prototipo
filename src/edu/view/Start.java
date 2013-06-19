e09d0e83036ee9a33b2a4a344c17a2839d4b88da
David Camilo Nova Fonseca <dnova@icck.net>
2013-06-19 09:09:09 -0500

    bandeja de documentos

--- src/edu/view/Start.java
+++ src/edu/view/Start.java
@@ -1,5 +1,6 @@
 package edu.view;
 
+import edu.logic.User;
 import edu.view.login.LoginFrame;
 import javax.swing.JFrame;
 
@@ -15,8 +16,8 @@ public class Start {
      * @param args
      */
     public static void main(String args[]) {
-//    FrameClient frame = new FrameClient(800, 600, "Ventana de prueba", 200, 50, User.getUser());
-    JFrame frame2 = new LoginFrame(350, 250, "Ventana de prueba", 200, 50);
+    FrameClient frame = new FrameClient(800, 600, "Ventana de prueba", 200, 50, User.getUser());
+//    JFrame frame2 = new LoginFrame(350, 250, "Ventana de prueba", 200, 50);
   }
   
 }
